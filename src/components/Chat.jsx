8b759770418cc946a46aeb336cc4cb75840fa183
adeldiaz <152525599+adeldiaz@users.noreply.github.com>
2024-03-07 11:46:52 -0600

    ajustes css
    -Position de mensaje y iniciador de usuario han sido ajustados 
    -Botón enviar ha sido colocado dentro de el input
    -index++ se suma después de la función setTimeout ha sido realizada para mostrar los mensajes desde el index 0 consecutivamente

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -28,15 +28,21 @@ const ChatComponent = () => {
         messageRef.current.value = '';
 
 
-        index++
+       
         console.log(index)
 
         setTimeout(() => {
-            setMessages(prevMessages => [...prevMessages, { text: predefinedResponses[index], from: 'bot' }]);
+            setMessages(prevMessages => [...prevMessages, 
+                { text: predefinedResponses[index],
+                     from: 'bot' 
+                    }]);
 
         }, 500)
-
+    
+        index++
     };
+
+let estaEscribiendo = false
 
     return (
         <>
@@ -51,15 +57,21 @@ const ChatComponent = () => {
 
                         <>
 
-                            {message.from === "user" && <div className="w-10 h-10 rounded-full bg-gray-800"></div>}
-
-                            <li key={index} className={`py-4 px-4 rounded-3xl border border-black my-4 w-fit ${message.from === 'user' ? 'text-left bg-white text-black mr-auto' : 'text-right bg-black text-white ml-auto'}`}>
+                            {message.from === "user" &&
+                            
+                            
+                            <div ></div>
+                            }
+                            <div className='overflow-hidden	'>
+                             <div className={ `w-10 h-10 rounded-full mt-5 ${message.from === 'user' ? `float-left bg-gray-200` : `float-right bg-black`}	`}></div>
+                          
+                            <li key={index} className={`py-4 px-4 rounded-3xl border border-black my-4 w-fit 	float-left	 ${message.from === 'user' ? 'text-left bg-white text-black mr-auto ml-5' : 'text-right bg-black text-white ml-60   float-right	'}`}>
 
                                 {message.text}
                                 {message.from === "bot" && <span className="w-10 h-10 rounded-full bg-gray-800"></span>}
 
                             </li>
-
+                            </div>
 
                         </>
 
@@ -67,17 +79,24 @@ const ChatComponent = () => {
 
                     ))}
                 </ul>
-                <form onSubmit={handleSubmit} className="mt-4">
+                <div className='flex justify-end items-center relative block'>
+                <div className="search-box mx-auto my-auto w-full sm:w-full md:w-full lg:w-3/4 xl:w-3/4 ">
+                <form onSubmit={handleSubmit} className="mt-4 flex flex-row">
+               {/*    ajustes css    */ }
                     <input
                         type="text"
                         ref={messageRef}
                         placeholder="Escribe un mensaje..."
-                        className="w-full py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                        className="w-full min-h-[50px] py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 	ml-4 pl-3 -mr-80"
                     />
-                    <button type="submit" className="mt-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg focus:outline-none">
-                        Enviar
+                     
+                    <button type="submit" className="mb-5 w-10 h-10  bg-black hover:bg-blue-600 text-white py-2 px-6 rounded-lg focus:outline-none m-5 ml-60	">
+                        
                     </button>
+
                 </form>
+                </div>
+                </div>
             </div>
         </>
 
@@ -86,3 +105,4 @@ const ChatComponent = () => {
 };
 
 export default ChatComponent;
+
